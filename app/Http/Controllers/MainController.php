7c06380c258ec84fade0ad5515b36727a630b8a7
Mayes-Art <mayes0art@gmail.com>
2020-12-14 01:04:06 +0800

    新增路由

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,20 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class MainController extends Controller
 {
     public function lineGet()
     {
         try {
-
+            $event = request()->all();
+            Log::info($event);
         } catch (\Exception $e) {
             report($e);
         }
     }
 
-    public function linePost()
+    public function linePost(Request $request)
     {
-
+        try {
+            $event = $request->all();
+            Log::info($event);
+        } catch (\Exception $e) {
+            report($e);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Route::get('info', function () {
     phpinfo();
 });
+
+Route::get('/', 'MainController@lineGet');
+
+Route::post('/', 'MainController@linePost');
