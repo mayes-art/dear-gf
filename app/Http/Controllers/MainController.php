3cfa729d6f73c8e11b20827d12bf86dad16fbdab
DESKTOP-LPFIQOH\darkm <mayes0art@gmail.com>
2020-12-13 23:47:32 +0800

    project init

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function lineGet()
+    {
+        try {
+
+        } catch (\Exception $e) {
+            report($e);
+        }
+    }
+
+    public function linePost()
+    {
+
+    }
+}
