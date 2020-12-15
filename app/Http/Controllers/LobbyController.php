eac491acc86e212c6686a1ed07019e52529927fd
Mayes <mayes0art@gmail.com>
2020-12-15 17:50:20 +0800

    新增LineBot服務層

--- app/Http/Controllers/LobbyController.php
+++ app/Http/Controllers/LobbyController.php
@@ -6,16 +6,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use LINE\LINEBot;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 
 class LobbyController extends Controller
 {
 
     protected $bot;
+    protected $messageBuilder;
 
     public function __construct()
     {
         $httpClient = new CurlHTTPClient(env('LINE_BOT_CHANNEL_ACCESS_TOKEN'));
         $this->bot = new LINEBot($httpClient, ['channelSecret' => env('LINE_BOT_CHANNEL_SECRET')]);
+        $this->messageBuilder = new MultiMessageBuilder();
     }
 
     public function lineGet()
@@ -37,7 +41,7 @@ class LobbyController extends Controller
             Log::info(json_encode($event));
             logger(json_encode($event, JSON_UNESCAPED_UNICODE));
 
-
+            $text = new TextMessageBuilder('歐嘍上帝, 阿們');
             $message = '歐嘍上帝, 阿們';
             $this->bot->replyText($event['events'][0]['replyToken'], $message);
 //            return response('test');
--- app/Services/LineBotService.php
+++ app/Services/LineBotService.php
@@ -4,7 +4,32 @@
 namespace App\Services;
 
 
+use Illuminate\Support\Facades\Log;
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+
 class LineBotService
 {
+    protected $bot;
 
+    protected $multiMessageBuilder;
+
+    public function __construct(MultiMessageBuilder $multiMessageBuilder)
+    {
+        $httpClient = new CurlHTTPClient(env('LINE_BOT_CHANNEL_ACCESS_TOKEN'));
+        $this->bot = new LINEBot($httpClient, ['channelSecret' => env('LINE_BOT_CHANNEL_SECRET')]);
+        $this->multiMessageBuilder = $multiMessageBuilder;
+    }
+
+    public function reply()
+    {
+        $response = $this->bot->replyMessage($this->multiMessageBuilder);
+        if (!$response->isSucceeded()) {
+            Log::error(json_encode([
+                'status' => $response->getHTTPStatus(),
+                'detail' => $response->getRawBody(),
+            ]));
+        }
+    }
 }
