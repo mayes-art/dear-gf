8d8dfb7634270387d63c78ff84806e2ce601bcba
Mayes <mayes0art@gmail.com>
2021-01-08 23:28:47 +0800

    加入匿稱map

--- app/Constants/LineUserMap.php
+++ app/Constants/LineUserMap.php
@@ -10,5 +10,6 @@ class LineUserMap
         'U014f9b64d9c6cedb4b4fc74f5f0ff0c2' => '阿偉',
         'U358b760aa9315b73b5bf905c59815b92' => '娃娃',
         'U62cae3a74a45582cfe3a9d7a34e2fb28' => '秀鑾',
+        'Udbdf1f44701316e02ba93d1ec848d0b3' => '阿轟',
     ];
 }
