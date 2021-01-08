786c65994481661a59f4aa8d0941d22500b061c1
Mayes <mayes0art@gmail.com>
2021-01-08 23:20:57 +0800

    修正匿稱map

--- app/Constants/LineUserMap.php
+++ app/Constants/LineUserMap.php
@@ -9,6 +9,6 @@ class LineUserMap
     const NICKNAME = [
         'U014f9b64d9c6cedb4b4fc74f5f0ff0c2' => '阿偉',
         'U358b760aa9315b73b5bf905c59815b92' => '娃娃',
-        'Ua9dc57b9f756bfd9fde3b42e8cea0ec2' => '秀鑾',
+        'U62cae3a74a45582cfe3a9d7a34e2fb28' => '秀鑾',
     ];
 }
